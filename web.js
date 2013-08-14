227d12dc474fa4bde95b3e8a43d2cd3f1bf1743d
Ubuntu <ubuntu@ip-10-216-23-46.us-west-2.compute.internal>
2013-08-14 20:34:20 +0000

    Modify the web.js

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+
+var data = fs.readFileSync("index.html","utf-8");
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2 from Tao Wang!');
+  response.send(data);
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
