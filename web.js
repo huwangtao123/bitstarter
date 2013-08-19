5ff9ac784528bc18f849889eaa8a2441f744005b
Ubuntu <ubuntu@ip-10-252-68-12.us-west-2.compute.internal>
2013-08-19 18:08:26 +0000

    Edited product in index.html

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = 'index.html'
 
-var data = fs.readFileSync("index.html","utf-8");
-
-var app = express();
-app.use(express.logger());
+var app =express();
 
 app.get('/', function(request, response) {
-  response.send(data);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
